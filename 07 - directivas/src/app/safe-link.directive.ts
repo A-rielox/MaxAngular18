1b7efc61caeafb500fcdb6d4c10e3acdf7e0d69b
A-rielox <arielox.ag@gmail.com>
2024-06-24 20:45:30 -0600

    8 pipes

--- 07 - directivas/src/app/safe-link.directive.ts
+++ 07 - directivas/src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, inject, input } from '@angular/core';
 
 @Directive({
    selector: 'a[appSafeLink]',
@@ -8,14 +8,29 @@ import { Directive } from '@angular/core';
    },
 })
 export class SafeLinkDirective {
-   constructor() {
-      console.log('safe link directive is active!');
-   }
+   // queryParam = input('myapp'); // 'myapp' va a ser el valor default
+   // p' ocuparlo como : <a href="https://angular.dev" appSafeLink queryParam="myapp-docs-link"
+
+   queryParam = input('myapp', { alias: 'appSafeLink' });
+   // p' ocuparlo como: <a href="https://angular.dev" appSafeLink="myapp-docs-link"
+
+   private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef); // p' tener acceso al elemento desde el q se llama ( el <a> )
 
    onConfirmLeavePage(event: MouseEvent) {
       const wantsToLeave = window.confirm('Do you want to leave the page ?'); // retorna true o false
 
-      if (wantsToLeave) return;
+      if (wantsToLeave) {
+         // const address = (event.target as HTMLAnchorElement).href;
+         // (event.target as HTMLAnchorElement).href =
+         //    address + '?from=' + this.queryParam();
+
+         // con el elementRef
+         const address = this.hostElementRef.nativeElement.href;
+         this.hostElementRef.nativeElement.href =
+            address + '?from=' + this.queryParam();
+
+         return;
+      }
 
       event.preventDefault();
    }
